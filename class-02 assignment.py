7cd026195f17204bf5d07d6ed00858f6e3cdc147
tanvirhasan91 <163386945+tanvirhasan91@users.noreply.github.com>
2024-03-14 12:55:21 +0600

    Add files via upload

---
+++ class-02 assignment.py
@@ -0,0 +1,33 @@
+ print("Md. Tanvir Hasan")
+
+batch=input("Enter your batch number:")
+print(batch)
+
+
+a='Bangladesh'
+b='India'
+c='Pakistan'
+d='Nepal'
+e= 'Japan'
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+
+BestPythonCourse="studymart"
+
+a=b=c="python"
+print(a)
+print(b)
+print(c)
+
+float=input("Enter a float value")
+print(float)
+
+a="welcome to Django web and Ai"
+print(a.upper())
+
+b="           python"
+print(b.strip())
